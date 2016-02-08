da9fe64e5acbdcae72d6ed779e2805db3fe77331
admiralronton <admiralronton@gmail.com>
2016-02-07 17:18:52 -0800

    Added Fog prefab and to map

--- Assets/Scripts/Environment/Sun.cs
+++ Assets/Scripts/Environment/Sun.cs
@@ -6,7 +6,7 @@ public class Sun : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        transform.RotateAround(Vector3.zero, Vector3.right, 10f * Time.deltaTime);
+        transform.RotateAround(Vector3.zero, Vector3.forward, 10f * Time.deltaTime);
         transform.LookAt(Vector3.zero);
 	}
 }
